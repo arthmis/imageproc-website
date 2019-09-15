cbe03445750c42372996c84ff41af01677d7dfba
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-09-15 19:28:06 -0400

    Finally correctly implemeneted scaling of image dimensions to canvas

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 import { invert, resize_img, default as init } from "../wasm/image_processing.js";
-async function run() {
+async function main() {
     // web assembly initialization
     // const imageproc = await import('../../image_processing/pkg');
     await init();
@@ -31,39 +31,15 @@ async function run() {
     let output_img = new Image();
     let output_img_data = new ImageData(1, 1);
     let original_img_data = new ImageData(1, 1);
-    // let canvas_img = new Image();
-    // let resized_img_width = 0;
-    // let resized_img_height = 0;
     let center_x = 0;
     let center_y = 0;
 
-    function display_image(canvas, img) {
+    function display_image(canvas, html_img) {
         let ctx = canvas.getContext("2d");
-        let resized_img_width = 0;
-        let resized_img_height = 0;
         // this sequence scales the image up or down to fit into canvas element
         // consider scaling to a percentage of the canvas
-        let scale = 1; 
-        // temporary solution to image scaling incorrectly, have to analyze this more
-        if (canvas.offsetHeight > img.height && canvas.offsetWidth > img.width) {
-            resized_img_height = img.height;
-            resized_img_width = img.width;
-        } else {
-
-            let width_scale = canvas.offsetHeight / img.height;
-            let height_scale = canvas.offsetWidth / img.width ;
-            if (width_scale < height_scale) {
-                scale = width_scale;
-            } else {
-                scale = height_scale;
-            }
-
-            resized_img_width = img.width * scale; 
-            resized_img_height = img.height * scale; 
-
-            resized_img_height = Math.round(resized_img_height);
-            resized_img_width = Math.round(resized_img_width);
-        }
+        const [resized_img_width, resized_img_height] = 
+            scale_img_dimensions_to_canvas(html_img, canvas);
 
         // resize the canvas to fit into canvas element size
         canvas.width = canvas.offsetWidth;
@@ -74,7 +50,7 @@ async function run() {
         center_y = (canvas.height - resized_img_height) / 2;
 
         ctx.drawImage(
-            img, 
+            html_img, 
             center_x, 
             center_y, 
             resized_img_width, 
@@ -82,31 +58,35 @@ async function run() {
         );
         
     }
+
     function scale_img_dimensions_to_canvas(img, canvas) {
 
         let scale = 0; 
         let new_height = 0;
         let new_width = 0;
 
-        if (img.width > img.height) {
-            scale = canvas.offsetWidth / img.width ;
+        const width_scale = canvas.offsetWidth / img.width;
+        const height_scale = canvas.offsetHeight / img.height;
+
+        if (width_scale < height_scale) {
+            scale = width_scale;
         } else {
-            scale = canvas.offsetHeight / img.height;
+            scale = height_scale; 
         }
 
         if (canvas.offsetWidth < img.width) {
             new_width = img.width * scale; 
         }
-        if (input_canvas.offsetHeight < img.height) {
+        if (canvas.offsetHeight < img.height) {
             new_height = img.height * scale; 
         }
 
         new_height = Math.round(new_height);
         new_width = Math.round(new_width);
-        return [new_width, new_height]
+        return [new_width, new_height];
     }
-    function put_image_data(img_data, ctx) {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    function put_image_data_canvas(img_data, ctx) {
         // get new image dimensions to fit on canvas
         const [resized_img_width, resized_img_height] = 
             scale_img_dimensions_to_canvas(img_data, ctx.canvas);
@@ -136,37 +116,27 @@ async function run() {
             resized_img_height, 
         );
     }
+
     let invert_button = document.getElementById("invert");
     invert_button.addEventListener("click", function() {
-        // let output_data = raw_output_ctx.getImageData(
-        //     0, 0, original_img.width, original_img.height
-        // );
         let input_data = output_img_data;
         let output_width = input_data.width;
         let raw_data = new Uint8ClampedArray(
             invert(input_data.data, output_width),
         );
         output_img_data = new ImageData(raw_data, output_width); 
-        put_image_data(output_img_data, output_ctx);
+        put_image_data_canvas(output_img_data, output_ctx);
     });
+
     // extracted this because its used in the on drop and file dialog click events
     function import_and_display(image_url) {
         original_img.addEventListener('load', function () {
             output_img = original_img;
 
-            // console.log(original_img);
-            // console.log("original width", original_img.width);
-            // console.log("original height", original_img.height);
-
-
-            // TODO possibly rename this because the image isn't literally scaled
-            // but the new scaled height and width are returned
             const [resized_img_width, resized_img_height] = 
                 scale_img_dimensions_to_canvas(original_img, input_canvas);
 
             // resize the canvas to fit into canvas element size
-            // console.log("canvas offset width", input_canvas.offsetWidth);
-            // console.log("canvas offset height", input_canvas.offsetHeight);
             input_canvas.width = input_canvas.offsetWidth;
             input_canvas.height = input_canvas.offsetHeight;
             output_canvas.width = input_canvas.width;
@@ -175,11 +145,6 @@ async function run() {
             // these two variables center the image within the canvas
             center_x = (input_canvas.width - resized_img_width) / 2;
             center_y = (input_canvas.height - resized_img_height) / 2;
-            // console.log("canvas width", input_canvas.width);
-            // console.log("canvas height", input_canvas.height);
-            // console.log("resized width", resized_img_width);
-            // console.log("resized height", resized_img_height);
-
 
             display_image(input_canvas, original_img);
             display_image(output_canvas, output_img);
@@ -213,8 +178,9 @@ async function run() {
         original_img.src = window.URL.createObjectURL(image_url);
 
     }
+
     let file_input = document.getElementById("file-input");
-    file_input.addEventListener("change", (event) => {
+    file_input.addEventListener("change", () => {
         let image = file_input.files[0];
         import_and_display(image);
         file_input.value = null;
@@ -228,8 +194,8 @@ async function run() {
         output_canvas.width = input_canvas.width;
         output_canvas.height = input_canvas.height;
 
-        put_image_data(original_img_data, input_ctx);
-        put_image_data(output_img_data, output_ctx);
+        put_image_data_canvas(original_img_data, input_ctx);
+        put_image_data_canvas(output_img_data, output_ctx);
     });
 
     let invert_option = document.getElementById("invert-option");
@@ -278,4 +244,4 @@ async function run() {
     });
 }
 
-run();
+main();
