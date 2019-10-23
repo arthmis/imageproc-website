9f6c370890b4749d9a0d3b1df6111fbc97e52072
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-10-23 17:05:39 -0400

    added debouncing and throttle functions to help create a more responsive ui when using web worker

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,36 @@
 import { DrawCanvases } from "./draw_canvases.js";
 import { RawImage } from "./raw_image.js";
 
+const debounce = (func, delay) => {
+    let inDebounce;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(inDebounce);
+        inDebounce = setTimeout(() => func.apply(context, args), delay);
+    }
+}
+
+const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan;
+    return function() {
+        const context = this;
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+        clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    }
+}
 async function main() {
     let image_worker = new Worker("./js/bundle_worker.js");
     image_worker.onmessage = event => {
